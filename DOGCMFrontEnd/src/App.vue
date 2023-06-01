aca4a649f87198c7422a9a486860baffb51a5f28
zoransy <zoransy@outlook.com>
2023-06-01 23:31:59 +0800

    update .gitignore

---
+++ DOGCMFrontEnd/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="building">
+  <transition name="transitionRouter">
+    <router-view></router-view>
+  </transition>
+    </div>
+</template>
+
+<script>
+
+export default {
+  // name: 'app',
+  // components: {
+  //   Login,
+  //   // HelloWorld
+  // }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+body{
+  margin:0;
+  padding:0;
+  border:0
+}
+#building {
+  background: url("~@/assets/background.jpg");
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  background-size: cover;
+  background-position: 0px -70px;
+}
+</style>
